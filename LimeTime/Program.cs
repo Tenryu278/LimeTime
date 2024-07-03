f7b59f5e66ec4afd7a6d5d6f520bfa239139505d
Tenryu278 <170785358+Tenryu278@users.noreply.github.com>
2024-07-03 14:54:24 +0900

    Add app.manifest

--- LimeTime/Program.cs
+++ LimeTime/Program.cs
@@ -16,13 +16,8 @@ namespace LimeTime
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            SetProcessDPIAware();
             Application.Run(new MainForm());
         }
-
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        private static extern bool SetProcessDPIAware();
-
         
     }
 }
